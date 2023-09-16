07dc8f1b8d68c7bf31aa050ba2fc872f550c4314
zero <1684713951@qq.com>
2023-09-16 17:22:21 +0800

    第一版 IM、PC设计，ID设计中

---
+++ user/src/ID.v
@@ -0,0 +1,13 @@
+module ID(
+    input               clk_sys_i      ,
+    input [31:0] instr_i,
+    output [31:0] rst1,
+    output [31:0] rst2,
+    output [31:0] rd,
+
+);
+    always @(posedge clk_sys_i) begin
+        ;
+    end
+
+endmodule //ID
